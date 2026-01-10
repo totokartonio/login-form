2fc7797c958b1fcc6cb1e8a39b3178e7f818c0a3
Anton Efimchenko <efimchenkoa@gmail.com>
2026-01-10 13:59:32 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Login form
+
+Under construction.
---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1>Login Form</h1>
+      <p>Hello World!</p>
+    </>
+  );
+}
+
+export default App;
