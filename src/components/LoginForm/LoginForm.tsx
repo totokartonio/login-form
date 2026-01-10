b5dbca5b5f3f7d5dad78717151de7fac98f95b69
Anton Efimchenko <efimchenkoa@gmail.com>
2026-01-10 15:50:38 +0100

    feat: add state management and form submission handling
    - Implement useState for form data (email, password)
    - Add handleChange to update form state on input change
    - Add handleSubmit to handle form submission with preventDefault

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,9 +1,25 @@
 import styles from "./LoginForm.module.css";
+import { useState } from "react";
+import type { FormData } from "../../type";
 import Input from "../Input";
 
 const LoginForm = () => {
+  const [formData, setFormData] = useState<FormData>({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Submitted: ", formData);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <Input
         label="Email"
         id="email"
@@ -12,6 +28,8 @@ const LoginForm = () => {
         placeholder="example@yourmail.com"
         autoComplete="username"
         required
+        value={formData.email}
+        onChange={handleChange}
       />
       <Input
         label="Password"
@@ -20,6 +38,8 @@ const LoginForm = () => {
         name="password"
         autoComplete="current-password"
         required
+        value={formData.password}
+        onChange={handleChange}
       />
       <button type="submit">Login</button>
     </form>
